09a02a112b18a351a904a5810383a509cc939fb9
Robby McKilliam <robby.mckilliam@gmail.com>
2015-07-07 11:04:03 +0930

    added start of QML

---
+++ code/java/src/poly/QML.java
@@ -0,0 +1,17 @@
+package poly;
+
+/**
+ * This is the Quasi maximum likelihood (QML) estimator from
+ * 
+ * Djurovic, I., Simeunovic, M., "Resolving aliasing effect in the QML estimation of PPSs"
+ * 
+ * Makes and estimate of the instantaneous frequency using the short term Fourier
+ * transform and applies phase unwrapping to this.
+ * 
+ * @author Robby McKilliam
+ */
+public class QML {
+    
+    public QML(int N, int m, int[] H)
+    
+}
