dd7f74f5c26d7e2d593e4903a290f41dbf21cc10
Robby McKilliam <robby.mckilliam@gmail.com>
2015-07-08 16:14:34 +0930

    added to QML. This actually appears to work!

--- code/java/test/poly/QMLTest.java
+++ code/java/test/poly/QMLTest.java
@@ -1,8 +1,14 @@
 package poly;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.After;
@@ -78,22 +84,44 @@ public class QMLTest {
         System.out.println("Write stft to file");
         int N = 256;
         double h =12;
-        double[] b = {1.0/10, -2.0/10, 1.0/200, 1.0/50000};
-        PolynomialPhaseSignal siggen = new PolynomialPhaseSignal(N, new Gaussian(0,0), b);
+        double[] b = {1.0/10, -2.0/10, -1.0/200, 1.0/50000};
+        //double noisevar = 1.0/2.0; //SNR = 1
+        double noisevar = 0.0; //SNR is infinite
+        PolynomialPhaseSignal siggen = new PolynomialPhaseSignal(N, new Gaussian(0,noisevar), b);
         siggen.generateReceivedSignal();
         Complex[] y = new Complex[N];
         for(int n = 0; n < N; n++) y[n] = new Complex(siggen.real()[n], siggen.imag()[n]);
-        try {
-            FileWriter fw = new FileWriter(new File("testdata/stftimageplotdata"));
-            double fstep = 1.0/h/4; //4 times oversampling in frequency dimension
-            for(double f = -0.5; f < 0.5; f += fstep) {
-                for(int t = 0; t < N-1; t++) fw.write(QML.stft(t, f, h, y).abs() + "\t");
-                fw.write(QML.stft(255, f, h, y).abs() + "\n");
+        try { //write STFT to a binary format suitable for gnuplot
+            FileOutputStream fos = new FileOutputStream("testdata/stftimageplotdata");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            writeFloatToStream(bos,N);
+            for(int t = 0; t < N; t++) writeFloatToStream(bos,t);
+            double fstep = 1.0/h/4.0; //4 times oversampling in frequency dimension
+            for(double f = 0.5; f > -0.5; f -= fstep) {
+                writeFloatToStream(bos,(float)f);
+                for(int t = 0; t < N; t++) writeFloatToStream(bos,(float)QML.stft(t, f, h, y).abs());
             }
+            bos.close();
+        } catch (IOException ex) {
+            fail("Failed to open file");
+        }
+        
+        try { //write maximiser of STFT to file
+            BufferedWriter file = new BufferedWriter(new FileWriter(new File("testdata/stftmax")));
+             for(int t = 0; t < N; t++) file.write(t + "\t" + QML.max_stft(t, h, y) + "\n");
+             file.close();
         } catch (IOException ex) {
             fail("Failed to open file");
         }
         
       }
+
+    //Write in little endian format (stupid java)
+    public static void writeFloatToStream(BufferedOutputStream bos, float f) throws IOException{
+        ByteBuffer bbuf = ByteBuffer.allocate(4);
+        bbuf.order(ByteOrder.LITTLE_ENDIAN);
+        bbuf.putFloat(f);
+        bos.write(bbuf.array());
+    }
     
 }
--- code/java/testdata/stftimageplotdata.gnuplot
+++ code/java/testdata/stftimageplotdata.gnuplot
@@ -1,15 +1,8 @@
 set title "STFT and maximiser"
-unset key
-set tic scale 0
-
-# Color runs from white to green
-set palette rgbformula -7,2,-7
-set cbrange [0:5]
-set cblabel "Magnitude"
-unset cbtics
-
 set xrange [0:255]
 set yrange [-0.5:0.5]
+set terminal pdf
+set output "stftimage.pdf"
 
-set view map
-splot 'stftimageplotdata' matrix with image
+plot 'stftimageplotdata' binary matrix with image, \
+"stftmax" with lines
